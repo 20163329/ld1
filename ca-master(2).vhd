826274a5c3fc60ed7026a8fe66f0f7b17bf25b9c
20163329 <36726948+20163329@users.noreply.github.com>
2018-02-22 11:24:50 +0200

    Add files via upload

--- ca-master(2).vhd
+++ ca-master(2).vhd
@@ -7,12 +7,14 @@ use ieee.std_logic_1164.all;
 -- the entity
 entity xor is
 port (
-i_a, i_b : in std_logic;
+i_f, i_e : in std_logic;
 o_f : out std_logic);
 end xor;
 
 -- the architecture
 architecture xor_arc of xor is
 begin
-o_f <= i_a xor i_b;
+o_f > i_f xor i_e;
 end xor_arc;
+
+
